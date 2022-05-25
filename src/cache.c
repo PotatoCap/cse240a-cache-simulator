60d488c4c37b4996ea07703c38402bce10b2da40
Valerie Liu <lilacwine373@gmail.com>
2022-05-25 02:29:49 -0700

    added the general idea. still not tested.

--- src/cache.c
+++ src/cache.c
@@ -7,6 +7,7 @@
 //========================================================//
 
 #include "cache.h"
+#include <stdio.h>
 
 //
 // TODO:Student Information
@@ -58,20 +59,20 @@ uint64_t l2cachePenalties; // L2$ penalties
 //
 uint32_t offset_bits;
 uint32_t i_index_bits;
-uint32_t i_tag_bits;
 uint32_t d_index_bits;
-uint32_t d_tag_bits;
 uint32_t l2_index_bits;
-uint32_t l2_tag_bits;
 
 struct block {
   uint32_t tag;
   int has_data;
+  int lru;
 };
 
 struct block** icache;
 struct block** dcache;
 struct block** l2cache;
+
+enum cacheType {ic, dc, l2};
 //
 
 //------------------------------------//
@@ -79,13 +80,66 @@ struct block** l2cache;
 //------------------------------------//
 
 uint32_t intlog2 (uint32_t n) {
-  if (n == 0 ) return 0;
+  if (n == 0) return 0;
   uint32_t result = 0;
   while (n > 1) {
     n >>= 1;
     result++;
   }
   return result;
+}
+
+uint32_t getIndex(uint32_t addr, uint32_t index_bits){
+  uint32_t higher_bits = addr >> offset_bits;
+  uint32_t index_mask = (1 << index_bits) - 1;
+  return higher_bits && index_mask;
+}
+
+uint32_t getTag(uint32_t addr, uint32_t index_bits){
+  return addr >> (offset_bits + index_bits);
+}
+
+int readData(struct block* line, uint32_t assoc, uint32_t tag){
+  for (int i = 0; i < assoc; i++)
+  {
+    struct block data = line[i];
+    if (data.tag == tag)
+    {
+      return data.has_data;
+    }
+  }
+  return FALSE;
+}
+
+void updateLRU(struct block* line, uint32_t assoc, uint32_t tag){
+  for (int i = 0; i < assoc; i++)
+  {
+    struct block data = line[i];
+    if (data.tag == tag) {
+      data.lru = 0;
+    }else{
+      data.lru++;
+    }
+  }
+}
+
+void insertData(struct block* line, uint32_t assoc, uint32_t tag){
+  int maxlru = 0;
+  int maxarg = 0;
+  for (int i = 0; i < assoc; i++)
+  {
+    struct block data = line[i];
+    if (!data.has_data) {
+      maxarg = i;
+      break;
+    }else if(data.lru > maxlru) {
+      maxlru = data.lru;
+      maxarg = i;
+    }
+  }
+  line[maxarg].has_data = TRUE;
+  line[maxarg].tag = tag;
+  updateLRU(line, assoc, tag);
 }
 
 // Initialize the Cache Hierarchy
@@ -106,11 +160,8 @@ init_cache()
   
   offset_bits = intlog2(blocksize);
   i_index_bits = intlog2(icacheSets);
-  i_tag_bits = 32-i_index_bits-offset_bits;
   d_index_bits = intlog2(dcacheSets);
-  d_tag_bits = 32-d_index_bits-offset_bits;
   l2_index_bits = intlog2(l2cacheSets);
-  l2_tag_bits = 32-l2_index_bits-offset_bits;
 
   icache = malloc(icacheSets * sizeof(struct block*));
   for (int i = 0; i < icacheSets; i++)
@@ -118,7 +169,7 @@ init_cache()
     icache[i] = malloc(icacheAssoc * sizeof(struct block));
     for (int j = 0; j < icacheAssoc; j++)
     {
-      icache[i][j] = (struct block){0, 0};
+      icache[i][j] = (struct block){0, 0, 0};
     }
   }
 
@@ -128,7 +179,7 @@ init_cache()
     dcache[i] = malloc(dcacheAssoc * sizeof(struct block));
     for (int j = 0; j < dcacheAssoc; j++)
     {
-      dcache[i][j] = (struct block){0, 0};
+      dcache[i][j] = (struct block){0, 0, 0};
     }
   }
 
@@ -138,10 +189,48 @@ init_cache()
     l2cache[i] = malloc(l2cacheAssoc * sizeof(struct block));
     for (int j = 0; j < l2cacheAssoc; j++)
     {
-      l2cache[i][j] = (struct block){0, 0};
+      l2cache[i][j] = (struct block){0, 0, 0};
     }
   }
-  
+}
+
+int
+cache_access(enum cacheType type, uint32_t addr){
+  struct block** cache;
+  uint32_t assoc;
+  uint32_t index_bits;
+
+  switch (type) {
+    case ic:
+      cache = icache;
+      assoc = icacheAssoc;
+      index_bits = i_index_bits;
+      break;
+    case dc:
+      cache = dcache;
+      assoc = dcacheAssoc;
+      index_bits = d_index_bits;
+      break;
+    case l2:
+      cache = l2cache;
+      assoc = l2cacheAssoc;
+      index_bits = l2_index_bits;
+      break;
+    default:
+      printf("Error: unknown cache type %d\n", type);
+      exit(1);
+  }
+
+  uint32_t index = getIndex(addr, index_bits);
+  uint32_t tag = getTag(addr, index_bits);
+  struct block* line = cache[index];
+  int hit = readData(line, assoc, tag);
+  if (hit) {
+    updateLRU(line, assoc, tag);
+  } else {
+    insertData(line, assoc, tag);
+  }
+  return hit;
 }
 
 // Perform a memory access through the icache interface for the address 'addr'
@@ -150,10 +239,14 @@ init_cache()
 uint32_t
 icache_access(uint32_t addr)
 {
-  //
-  //TODO: Implement I$
-  //
-  return memspeed;
+  icacheRefs++;
+  int hit = cache_access(ic, addr);
+  if (hit) {
+    return icacheHitTime;
+  } else {
+    icacheMisses++;
+    return l2cache_access(addr);
+  }
 }
 
 // Perform a memory access through the dcache interface for the address 'addr'
@@ -162,10 +255,14 @@ icache_access(uint32_t addr)
 uint32_t
 dcache_access(uint32_t addr)
 {
-  //
-  //TODO: Implement D$
-  //
-  return memspeed;
+  dcacheRefs++;
+  int hit = cache_access(dc, addr);
+  if (hit) {
+    return dcacheHitTime;
+  } else {
+    dcacheMisses++;
+    return l2cache_access(addr);
+  }
 }
 
 // Perform a memory access to the l2cache for the address 'addr'
@@ -174,8 +271,12 @@ dcache_access(uint32_t addr)
 uint32_t
 l2cache_access(uint32_t addr)
 {
-  //
-  //TODO: Implement L2$
-  //
-  return memspeed;
+  l2cacheRefs++;
+  int hit = cache_access(l2, addr);
+  if (hit) {
+    return l2cacheHitTime;
+  } else {
+    l2cacheMisses++;
+    return memspeed;
+  }
 }
